e56e29900a4e43840145401a990ba19fe8ac1389
ChatNghe <namnt98@gmail.com>
2022-10-07 11:20:10 +0700

    thay doi 1

---
+++ demoscript.js
@@ -0,0 +1,6 @@
+function ShowMessage(){
+    <!--alert('Xin chào');-->
+    <!--document.getElementById('messageDiv').innerHTML="Xin chào";-->
+    document.write('Xin chào');
+    // console.log('Xin chào');
+}
